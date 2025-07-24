a69c3aa9677acb93c86249eca1685ea4e3099640
annastriw <annastriwidagdoo@gmail.com>
2025-07-24 07:22:00 +0700

    feat: implement module content and post-test locking if pre-test has not been taken

--- app/Models/SubModule.php
+++ app/Models/SubModule.php
@@ -28,19 +28,18 @@ class SubModule extends BaseModel
         return $this->hasMany(KM::class);
     }
 
-    public function moduleContents()
-{
-    return $this->hasMany(ModuleContent::class)->orderBy('created_at', 'asc');
-}
+    public function preTests()
+    {
+        return $this->hasMany(PreTest::class);
+    }
 
+    public function moduleContents()
+    {
+        return $this->hasMany(ModuleContent::class)->orderBy('created_at', 'asc');
+    }
 
     public function postTests()
     {
         return $this->hasMany(PostTest::class);
-    }
-
-    public function preTests()
-    {
-        return $this->hasMany(PreTest::class);
     }
 }
